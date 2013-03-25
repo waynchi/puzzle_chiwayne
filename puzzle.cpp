0d0a2b745c8dc614532fd216e82f1944500e6fb3
Wayne Chi <chiwayne@usc.edu>
2013-03-24 19:20:08 -0700

    Added a simple implementation. No cheats yet. Tested it and it worked. close #2 close #3

--- puzzle.cpp
+++ puzzle.cpp
@@ -14,13 +14,25 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  
   int size, initMoves, seed;
-
+  
   size = atoi(argv[1]);
   initMoves = atoi(argv[2]);
   seed = atoi(argv[3]);
 
   Board b(size,initMoves,seed);
+  int input;
+  while(!b.solved())
+  {
+  b.printBoard();
+  cout << "Please enter the number you would like to move" << endl;
+  cin >> input;
+  b.move(input);
+  }
+  b.printBoard();
+  cout << "Congratulations! You've solved the puzzle!" << endl;
+
 
   //**** Implement the gameplay here
 
